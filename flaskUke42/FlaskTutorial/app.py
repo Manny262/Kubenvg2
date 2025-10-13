31b43ab8c63a377df98777d2eff7c20cd1b86938
Manny262 <mawia031>
2025-10-13 13:21:24 +0200

    Ferdig med oppgave. neste oppgave er å koble til en database

---
+++ flaskUke42/FlaskTutorial/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('master.html')
+
+@app.route('/start')
+def page1():
+    list = [
+        'John', 'Per', 'Anne'
+    ]
+    return render_template('start.html', list = list)
+
+if __name__ == '__main__':
+    app.run(debug='True')
