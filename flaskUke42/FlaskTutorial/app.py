09d7612a246d5b6a9a6e4710c0301137e7da2f1a
Manny262 <mawia031>
2025-10-14 11:58:30 +0200

    fortsett flask +db

--- flaskUke42/FlaskTutorial/app.py
+++ flaskUke42/FlaskTutorial/app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URL'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+db = SQLAlchemy(app)
+
+class bruker(db.model):
+    id= db.Column(db.Serial, primary_key=True, not_null = True)
+    Fornavn = db.Column(db.string(50), not_null = True)
 
 @app.route('/')
 def hello():
@@ -8,9 +21,7 @@ def hello():
 
 @app.route('/start')
 def page1():
-    list = [
-        'John', 'Per', 'Anne'
-    ]
+    list = bruker.query.all()
     return render_template('start.html', list = list)
 
 if __name__ == '__main__':
